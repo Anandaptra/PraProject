fb42210a670b99d766daebdf9f1760ccdb69cf77
Rogo <rogosubandono513@gmail.com>
2023-05-23 21:57:46 +0700

    restfulapi

--- app/src/main/java/com/example/praproject/data/retrofit/RestfulApi.kt
+++ app/src/main/java/com/example/praproject/data/retrofit/RestfulApi.kt
@@ -1,4 +1,10 @@
 package com.example.praproject.data.retrofit
 
+import com.example.praproject.data.endpoint.DataUsersItem
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface RestfulApi {
+    @GET("DataUser")
+    fun getAllNews(): Call<List<DataUsersItem>>
 }
