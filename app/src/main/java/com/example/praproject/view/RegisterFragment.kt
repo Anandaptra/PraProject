87fde0e499536139bcaace142288cf0548557394
BAGAS <anndptra658@gmail.com>
2023-05-23 22:48:03 +0700

    Registrasi

--- app/src/main/java/com/example/praproject/view/RegisterFragment.kt
+++ app/src/main/java/com/example/praproject/view/RegisterFragment.kt
@@ -1,11 +1,14 @@
 package com.example.praproject.view
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.praproject.R
 import com.example.praproject.databinding.FragmentRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -14,7 +17,6 @@ import com.google.firebase.auth.FirebaseAuth
 class RegisterFragment : Fragment() {
     lateinit var binding: FragmentRegisterBinding
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var pref: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +27,34 @@ class RegisterFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnRegis.setOnClickListener {
+            registAccount()
+        }
+    }
+
+    private fun registAccount() {
+        val username = binding.etNama.text.toString()
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPw.text.toString()
+
+
+        if (username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(context, "Registration Success", Toast.LENGTH_SHORT).show()
+                    findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+                } else {
+                    Toast.makeText(context, "Registration Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
 
 
 }
